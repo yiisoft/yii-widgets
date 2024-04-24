d452c5198fdbaa726d71dba2a9eed12a7c7d70fb
Luiz Marin <67489841+luizcmarin@users.noreply.github.com>
2024-04-24 14:42:57 -0300

    Update layout.php

--- tests/Support/view/layout.php
+++ tests/Support/view/layout.php
@@ -5,22 +5,16 @@ declare(strict_types=1);
 /* @var $this Yiisoft\View\WebView */
 /* @var $content string */
 ?>
-<?php $this->beginPage(); ?>
-<!DOCTYPE html>
+<?php $this->beginPage(); ?><!DOCTYPE html>
 <html lang="en">
-
 <head>
     <title>Test</title>
     <?php $this->head(); ?>
 </head>
-
 <body>
-    <?php $this->beginBody(); ?>
-
-    <?= $content ?>
-
-    <?php $this->endBody(); ?>
+<?php $this->beginBody(); ?>
+<?= $content ?>
+<?php $this->endBody(); ?>
 </body>
-
 </html>
 <?php $this->endPage(); ?>
