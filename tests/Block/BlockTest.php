0aa54c98f681578fb07376709b0f6f78ebe712df
Sergei Predvoditelev <sergei@predvoditelev.ru>
2025-06-02 12:22:20 +0300

    Fix `Block` behavior when content is "0" (#97)

--- tests/Block/BlockTest.php
+++ tests/Block/BlockTest.php
@@ -5,26 +5,16 @@ declare(strict_types=1);
 namespace Yiisoft\Yii\Widgets\Tests\Block;
 
 use PHPUnit\Framework\TestCase;
-use Yiisoft\Definitions\Exception\CircularReferenceException;
-use Yiisoft\Definitions\Exception\InvalidConfigException;
-use Yiisoft\Definitions\Exception\NotInstantiableException;
-use Yiisoft\Factory\NotFoundException;
 use Yiisoft\Yii\Widgets\Block;
 use Yiisoft\Yii\Widgets\Tests\Support\TestTrait;
 
-/**
- * @psalm-suppress PropertyNotSetInConstructor
- */
+use function PHPUnit\Framework\assertFalse;
+use function PHPUnit\Framework\assertSame;
+
 final class BlockTest extends TestCase
 {
     use TestTrait;
 
-    /**
-     * @throws CircularReferenceException
-     * @throws InvalidConfigException
-     * @throws NotFoundException
-     * @throws NotInstantiableException
-     */
     public function testBlock(): void
     {
         Block::widget()->id('testme')->begin();
@@ -34,12 +24,6 @@ final class BlockTest extends TestCase
         $this->assertStringContainsString('<block-testme>', $this->webView->getBlock('testme'));
     }
 
-    /**
-     * @throws CircularReferenceException
-     * @throws InvalidConfigException
-     * @throws NotFoundException
-     * @throws NotInstantiableException
-     */
     public function testBlockRenderInPlaceTrue(): void
     {
         Block::widget()->id('testme')->renderInPlace()->begin();
@@ -47,5 +31,24 @@ final class BlockTest extends TestCase
         $html = Block::end();
 
         $this->assertStringContainsString('<block-testme>', $html);
+    }
+
+    public function testZeroContent(): void
+    {
+        Block::widget()->id('test')->begin();
+        echo '0';
+        $result = Block::end();
+
+        assertSame('', $result);
+        assertSame('0', $this->webView->getBlock('test'));
+    }
+
+    public function testEmptyContent(): void
+    {
+        Block::widget()->id('test')->begin();
+        $result = Block::end();
+
+        assertSame('', $result);
+        assertFalse($this->webView->hasBlock('test'));
     }
 }
