49ae651db717ef577aee303c0ca9c575df6edbbe
Sergei Predvoditelev <sergei@predvoditelev.ru>
2026-08-11 13:46:19 +0300

    fix

--- composer-dependency-analyser.php
+++ composer-dependency-analyser.php
@@ -4,8 +4,19 @@ declare(strict_types=1);
 
 use ShipMonk\ComposerDependencyAnalyser\Config\Configuration;
 
-return (new Configuration())
+$config = (new Configuration())
     ->disableComposerAutoloadPathScan()
     ->setFileExtensions(['php'])
     ->addPathToScan(__DIR__ . '/src', isDev: false)
     ->addPathToScan(__DIR__ . '/tests', isDev: true);
+
+// These functions are defined in vendor/yiisoft/html/src/test-functions.php, which is loaded
+// manually in tests/bootstrap.php and is not part of composer's "files" autoload, so the analyser
+// cannot find their declarations and reports them as unknown functions.
+$config->ignoreUnknownFunctions([
+    'Yiisoft\Html\IdGenerator\disableSeed',
+    'Yiisoft\Html\IdGenerator\enableSeed',
+    'Yiisoft\Html\IdGenerator\reset',
+]);
+
+return $config;
